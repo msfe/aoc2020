db3d163b43c7861bad0c21cea81ba897d545ad15
Mattias Folke <mattias.folke@gmail.com>
2020-12-11 14:45:55 +0100

    10 first half acceptable solution

--- 10.ts
+++ 10.ts
@@ -3,22 +3,10 @@ import fs = require('fs');
 
 
 const getAns1 = (input: number[]): number => {
-    let ones = 0;
-    let threes = 0;
-    const sortedInput = input.sort((a, b) => a > b ? 1 : -1);
-    console.log(`Sorted: ${JSON.stringify(sortedInput)}`);
-    for (let index = 1; index < input.length; index++) {
-        const previousElement = sortedInput[index - 1];
-        const element = sortedInput[index];
-        console.log(`Diff: ${element - previousElement}`);
-        if (element - previousElement == 1) {
-            ones++;
-        }
-        if (element - previousElement == 3) {
-            threes++;
-        }
-    }
-    console.log(`Ones: ${ones} Threes: ${threes}`);
+    input.sort((a, b) => a > b ? 1 : -1);
+    const diffMap = input.map((nbr, index) => index == 0 ? 0 : nbr - input[index - 1]);
+    let ones = diffMap.filter(nbr => nbr == 1).length;
+    let threes = diffMap.filter(nbr => nbr == 3).length;
     return ones * threes;
 }
 
