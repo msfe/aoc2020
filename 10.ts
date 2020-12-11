69b927b5f4d638b0d4e0f4fc423e8308b6d56c0d
Mattias Folke <mattias.folke@gmail.com>
2020-12-11 14:39:03 +0100

    somewhat better

--- 10.ts
+++ 10.ts
@@ -7,12 +7,6 @@ const getAns1 = (input: number[]): number => {
     let threes = 0;
     const sortedInput = input.sort((a, b) => a > b ? 1 : -1);
     console.log(`Sorted: ${JSON.stringify(sortedInput)}`);
-    if (sortedInput[0] == 1) {
-        ones++;
-    }
-    if (sortedInput[0] == 1) {
-        threes++;
-    }
     for (let index = 1; index < input.length; index++) {
         const previousElement = sortedInput[index - 1];
         const element = sortedInput[index];
@@ -24,6 +18,7 @@ const getAns1 = (input: number[]): number => {
             threes++;
         }
     }
+    console.log(`Ones: ${ones} Threes: ${threes}`);
     return ones * threes;
 }
 
@@ -46,6 +41,8 @@ async function readNumberInput(filename: string): Promise<number[]> {
 
 async function main() {
     const input: number[] = await readNumberInput('input10');
+    input.push(0);
+    input.push(input.reduce((max, nbr) => Math.max(max, nbr)) + 3);
     console.log(`Answer 1: ${getAns1(input)}`);
 }
 
