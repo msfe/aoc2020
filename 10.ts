31d5fbfbb29aef77e0d020fc8815fc77d9793a9e
Mattias Folke <mattias.folke@gmail.com>
2020-12-11 20:45:48 +0100

    improved read input

--- 10.ts
+++ 10.ts
@@ -1,11 +1,11 @@
 import readline = require('readline')
 import fs = require('fs');
+import path = require('path');
 
 const ansMap: number[] = [0, 1, 2, 4, 7, 13, 24]; //Paths based on number of continuous 1s. Ie. 111 => ansMap[3] = 4 = [111,12,21,3].length
 
 const getAns1 = (input: number[]): number => {
     const diffMap = getDiffMap(input)
-    console.log(`Position: ${diffMap}`);
     const ones = diffMap.filter(nbr => nbr == 1).length;
     const threes = diffMap.filter(nbr => nbr == 3).length;
     return ones * threes;
@@ -23,23 +23,8 @@ const getDiffMap = (input: number[]): number[] => {
     return input.map((nbr, index) => index == 0 ? 0 : nbr - input[index - 1]);
 }
 
-async function readNumberInput(filename: string): Promise<number[]> {
-    const fileStream = fs.createReadStream(filename);
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-
-    const input: number[] = [];
-    for await (const line of rl) {
-        input.push(Number(line));
-    }
-    return input;
-}
-
-
 async function main() {
-    const input: number[] = await readNumberInput('input10');
+    const input: number[] = fs.readFileSync(path.resolve(__dirname, 'input10')).toString().split('\r\n').map(s => parseInt(s));
     input.push(0);
     input.push(input.reduce((max, nbr) => Math.max(max, nbr)) + 3);
     console.log(`Answer 1: ${getAns1(input)}`);
