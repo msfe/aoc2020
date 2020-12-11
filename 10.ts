e10d5a1818147fd2ae43547128e52073ca2ea052
Mattias Folke <mattias.folke@gmail.com>
2020-12-11 14:32:53 +0100

    first part

---
+++ 10.ts
@@ -0,0 +1,59 @@
+import readline = require('readline')
+import fs = require('fs');
+
+
+const getAns1 = (input: number[]): number => {
+    let ones = 0;
+    let threes = 0;
+    const sortedInput = input.sort((a, b) => a > b ? 1 : -1);
+    console.log(`Sorted: ${JSON.stringify(sortedInput)}`);
+    if (sortedInput[0] == 1) {
+        ones++;
+    }
+    if (sortedInput[0] == 1) {
+        threes++;
+    }
+    for (let index = 1; index < input.length; index++) {
+        const previousElement = sortedInput[index - 1];
+        const element = sortedInput[index];
+        console.log(`Diff: ${element - previousElement}`);
+        if (element - previousElement == 1) {
+            ones++;
+        }
+        if (element - previousElement == 3) {
+            threes++;
+        }
+    }
+    return ones * threes;
+}
+
+
+async function readNumberInput(filename: string): Promise<number[]> {
+    const fileStream = fs.createReadStream(filename);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    const input: number[] = [];
+    for await (const line of rl) {
+        input.push(Number(line));
+    }
+    return input;
+}
+
+
+async function main() {
+    const input: number[] = await readNumberInput('input10');
+    console.log(`Answer 1: ${getAns1(input)}`);
+}
+
+main();
+
+
+
+
+
+
+
