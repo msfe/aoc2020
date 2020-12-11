0c74ece9f359c83bb345e850f4f5749f9d6d0446
Mattias Folke <mattias.folke@gmail.com>
2020-12-11 20:56:42 +0100

    fixed input on 5

--- 5.ts
+++ 5.ts
@@ -1,6 +1,10 @@
-import readline = require('readline')
+import path = require('path')
 import fs = require('fs');
 
+interface seat {
+    row: string;
+    col: string;
+}
 
 const getSeatValue = (input: seat): number => {
     const rowBin = input.row.replace(/F/g, '0').replace(/B/g, '1').trim();
@@ -8,28 +12,6 @@ const getSeatValue = (input: seat): number => {
     const colBin = input.col.replace(/L/g, '0').replace(/R/g, '1').trim();
     const colNr = parseInt(colBin, 2);
     return rowNr * 8 + colNr;
-}
-
-
-interface seat {
-    row: string;
-    col: string;
-}
-
-async function readNumberInput(filename: string): Promise<seat[]> {
-    const fileStream = fs.createReadStream(filename);
-
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-
-    const input: seat[] = [];
-    for await (const line of rl) {
-
-        input.push({ row: line.slice(0, 7), col: line.slice(7) });
-    }
-    return input;
 }
 
 const mySeat = (seatIds: number[]): number => {
@@ -42,9 +24,9 @@ const mySeat = (seatIds: number[]): number => {
     return -1;
 }
 
-
-async function main() {
-    const input: seat[] = await readNumberInput('input5');
+function main() {
+    const input: seat[] = fs.readFileSync(path.resolve(__dirname, 'input5')).toString().split('\r\n')
+        .map(line => { return { row: line.slice(0, 7), col: line.slice(7) } });
     const seatIds: number[] = input.map(seat => getSeatValue(seat));
     const answer1 = seatIds.reduce((max, current): number => { return Math.max(max, current) }, 0);
     console.log(`Answer 1: ${answer1}`);
