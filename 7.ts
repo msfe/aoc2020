65ad6c3a12b3a370f4d693a933ff013b2afd852f
Mattias Folke <mattias.folke@gmail.com>
2020-12-12 14:57:13 +0100

    refactored solution 7

--- 7.ts
+++ 7.ts
@@ -1,4 +1,4 @@
-import readline = require('readline')
+import path = require('path')
 import fs = require('fs');
 
 interface bagItem {
@@ -6,25 +6,17 @@ interface bagItem {
     amount: number
 }
 
-interface Dictionary<T> {
-    [Key: string]: T;
+interface bag {
+    type: string,
+    items: bagItem[]
 }
 
-async function readInput(filename: string): Promise<Dictionary<bagItem[]>> {
-    const fileStream = fs.createReadStream(filename);
-
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-
-    const bags: Dictionary<bagItem[]> = {}
-    for await (const line of rl) {
+function generateBagItemDictionary(input: string[]): bag[] {
+    return input.map(line => {
         const parts = line.split('contain');
         const bagItems: bagItem[] = getBagItems(parts[1]);
-        bags[trimItem(parts[0])] = bagItems;
-    }
-    return bags;
+        return { "type": trimItem(parts[0]), "items": bagItems };
+    })
 }
 
 const getBagItems = (input: string): bagItem[] => {
@@ -45,35 +37,30 @@ const trimItem = (item: string): string => {
     return item.trim().replace(/s*\.*$/g, '');
 }
 
-const getBagColors = (bagMap: Dictionary<bagItem[]>, bag: string): number => {
-    let res = 0;
-    for (let bagDescription in bagMap) {
-        res += canHoldThisBag(bagMap, bag, bagDescription) ? 1 : 0;
-    }
-    return res;
+const getBagColors = (bagMap: bag[], bag: string): number => {
+    const result: number[] = bagMap.map(curBag => canHoldThisBag(bagMap, bag, curBag.type) ? 1 : 0)
+    return result.reduce((acc, cur) => acc + cur, 0);
 }
 
-const canHoldThisBag = (bagMap: Dictionary<bagItem[]>, searchFor: string, searchIn: string): boolean => {
-    const possibilities = bagMap[searchIn] as bagItem[];
+const canHoldThisBag = (bagMap: bag[], searchFor: string, searchIn: string): boolean => {
+    const possibilities = bagMap.filter(bag => bag.type == searchIn)[0].items as bagItem[];
     if (possibilities.filter(({ description }) => description == searchFor).length > 0) {
         return true;
     }
-    return possibilities.map(({ description }) => canHoldThisBag(bagMap, searchFor, description)).some((value, index, array) => value == true);
+    return possibilities.map(({ description }) => canHoldThisBag(bagMap, searchFor, description)).some((value) => value == true);
 }
 
-const getBagsInBag = (bagMap: Dictionary<bagItem[]>, bag: string): number => {
-    const possibilities = bagMap[bag] as bagItem[];
-    return possibilities.map((bag) => bag.amount + bag.amount * getBagsInBag(bagMap, bag.description)).reduce((agr, current) => agr + current, 0);
+const getBagsInBag = (bagMap: bag[], searchIn: string): number => {
+    return bagMap.filter((bag) => bag.type == searchIn)[0].items.map((bag) => bag.amount + bag.amount * getBagsInBag(bagMap, bag.description)).reduce((agr, current) => agr + current, 0);
 }
 
 
 
-async function main() {
-    const input: Dictionary<bagItem[]> = await readInput('input7');
-    //console.log(JSON.stringify(input));
-    //const answer1 = seatIds.reduce((max, current): number => { return Math.max(max, current) }, 0);
-    console.log(`Answer 1: ${getBagColors(input, 'shiny gold bag')}`);
-    console.log(`Answer 2: ${getBagsInBag(input, 'shiny gold bag')}`);
+function main() {
+    const input: string[] = fs.readFileSync(path.resolve(__dirname, 'input7')).toString().split('\r\n');
+    const bagItemDictionary: bag[] = generateBagItemDictionary(input);
+    console.log(`Answer 1: ${getBagColors(bagItemDictionary, 'shiny gold bag')}`);
+    console.log(`Answer 2: ${getBagsInBag(bagItemDictionary, 'shiny gold bag')}`);
 }
 
 main();
