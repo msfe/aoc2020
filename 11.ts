266ce52408c8a7d956e9ceaf99774a10159e1729
msfe <mattias.folke@gmail.com>
2020-12-13 12:21:13 +0100

    completed 11

--- 11.ts
+++ 11.ts
@@ -12,7 +12,7 @@ const setCharAt = (str: string, index: number, chr: string): string => {
     return str.substring(0, index) + chr + str.substring(index + 1);
 }
 
-const isOccupied = (map: string[], row: number, col: number, dirRow: number, dirCol: number): boolean => {
+const isOccupied = (map: string[], row: number, col: number, dirRow: number, dirCol: number, assignment: number): boolean => {
     if (row == 0 && dirRow == -1)
         return false;
     if (col == 0 && dirCol == -1)
@@ -21,27 +21,38 @@ const isOccupied = (map: string[], row: number, col: number, dirRow: number, dir
         return false;
     if (col == map[row].length - 1 && dirCol == 1)
         return false;
+    
+    const tile = map[row + dirRow].charAt(col + dirCol);
+    if(assignment == 2){
+        if(tile == "#")
+            return true;
+        else if(tile == "L")
+            return false;
+        else
+            return isOccupied(map,row+dirRow,col+dirCol,dirRow,dirCol,assignment);
 
-    return map[row + dirRow].charAt(col + dirCol) == "#";
+    } else {
+        return tile == "#";
+    }
 }
 
 const numberOfOccupied = (map: string[], row: number, col: number, assignment: number): number => {
     let occupied = 0;
-    if (isOccupied(map, row, col, -1, -1))
+    if (isOccupied(map, row, col, -1, -1, assignment))
         occupied++
-    if (isOccupied(map, row, col, -1, 0))
+    if (isOccupied(map, row, col, -1, 0, assignment))
         occupied++
-    if (isOccupied(map, row, col, -1, 1))
+    if (isOccupied(map, row, col, -1, 1, assignment))
         occupied++
-    if (isOccupied(map, row, col, 0, -1))
+    if (isOccupied(map, row, col, 0, -1, assignment))
         occupied++
-    if (isOccupied(map, row, col, 0, 1))
+    if (isOccupied(map, row, col, 0, 1, assignment))
         occupied++
-    if (isOccupied(map, row, col, 1, -1))
+    if (isOccupied(map, row, col, 1, -1, assignment))
         occupied++
-    if (isOccupied(map, row, col, 1, 0))
+    if (isOccupied(map, row, col, 1, 0, assignment))
         occupied++
-    if (isOccupied(map, row, col, 1, 1))
+    if (isOccupied(map, row, col, 1, 1, assignment))
         occupied++
 
     return occupied;
@@ -79,6 +90,7 @@ const getStableCount = (mapState: map, assignment: number): number => {
 async function main() {
     const input: string[] = fs.readFileSync(path.resolve(__dirname, 'input11')).toString().split('\r\n');
     console.log(`Answer 1: ${getStableCount({ occupiedSeats: 0, map: input, isStable: false },1)}`);
+    console.log(`Answer 2: ${getStableCount({ occupiedSeats: 0, map: input, isStable: false },2)}`);
 }
 
 main();
