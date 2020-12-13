c88a9c3922ffa81a897f8e0b45b032ed6875c388
msfe <mattias.folke@gmail.com>
2020-12-13 16:16:53 +0100

    12 complete

--- 12.ts
+++ 12.ts
@@ -33,9 +33,54 @@ const getDistance = (input: string[]): number => {
     return Math.abs(westEast)+Math.abs(southNorth);
 }
 
+interface position {
+    x: number,
+    y: number,
+
+}
+
+const rotateClockwise = (waypoint: position): position => {
+    return {y: -waypoint.x, x:waypoint.y};
+}
+
+const getAns2 = (input: string[]): number => {
+    let boat: position = {x: 0, y: 0};
+    let wayPoint: position = {x: 10, y: 1};
+
+    for (let index = 0; index < input.length; index++) {
+        const instruction = input[index].charAt(0);
+        const amount = Number(input[index].substring(1));
+        if(instruction == "F"){
+            boat.x += wayPoint.x * amount;
+            boat.y += wayPoint.y * amount;
+        } else if(instruction == "L"){
+            wayPoint = rotateClockwise(wayPoint); // left at least 3
+            if(amount < 270)
+            wayPoint = rotateClockwise(wayPoint); // left at least 2
+            if(amount < 180)
+            wayPoint = rotateClockwise(wayPoint); // left == 1            
+        } else if(instruction == "R"){
+            for(let i = 0; i < amount/90; i++){
+                wayPoint = rotateClockwise(wayPoint);
+            }
+        } else if(instruction == "N"){
+            wayPoint.y += amount;
+        } else if(instruction == "S"){
+            wayPoint.y -= amount;
+        } else if(instruction == "E"){
+            wayPoint.x += amount;
+        } else if(instruction == "W"){
+            wayPoint.x -= amount;
+        }
+    }
+
+    return Math.abs(boat.x)+Math.abs(boat.y);
+}
+
 async function main() {
     const input: string[] = fs.readFileSync(path.resolve(__dirname, 'input12')).toString().split('\r\n');
     console.log(`Answer 1: ${getDistance(input)}`);
+    console.log(`Answer 1: ${getAns2(input)}`);
 }
 
 main();
